714c899e80d25c4d84e10b6a2933e4cb0301f169
German Espinosa <germanespinosa@gmail.com>
2023-06-26 13:41:45 -0500

    added default values to terminal for oasis task

--- terminal.py
+++ terminal.py
@@ -8,6 +8,7 @@ from json_cpp import JsonObject
 from gdrive import *
 
 clients = {'experiment': ExperimentClient(), 'maze1': PiClient(), 'maze2': PiClient()}
+
 ip = {'experiment': '127.0.0.1', 'maze1': '192.168.137.100', 'maze2': '192.168.137.200'}
 experiment_log_folder = "/research/data"
 # experiment_log_folder = "C:/Users/AlexT/OneDrive/Laptop Documents/Northwestern/[0] Research/Testing/"
